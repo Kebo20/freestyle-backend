0a7ddbf5f58bf2a5e1f2acd78c332c6f15578d11
Kevin Alberca <desarrollador.ext3@pymhse.com>
2021-05-01 12:33:26 -0500

    conf env

--- app/Http/Resources/Product.php
+++ app/Http/Resources/Product.php
@@ -17,7 +17,7 @@ class Product extends JsonResource
             'price_old' => htmlspecialchars(sprintf('%.2f',(htmlspecialchars($this->price_old)))),
             'brand' => htmlspecialchars($this->brand),
             'description' => htmlspecialchars($this->description),
-            'image' => $this->image?$this->image:"",
+            'image' => $this->image?env('APP_URL').$this->image:"",
             'novelty' => $this->novelty==1?true:false,
             'units' => htmlspecialchars($this->units),
             'idCategory' => $this->idCategory,
